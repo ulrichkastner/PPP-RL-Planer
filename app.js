e7fb6cb630faa1004ee9291674969962628b119b
Dr. Ulrich W. Kastner <ulrich.kastner@gmail.com>
2026-08-25 20:32:46 +0200

    Add module loader

---
+++ app.js
@@ -0,0 +1,15 @@
+(function(){
+  function load(src){
+    return new Promise(function(resolve,reject){
+      var s=document.createElement('script');
+      s.src=src;
+      s.onload=resolve;
+      s.onerror=function(){reject(new Error('Konnte '+src+' nicht laden'));};
+      document.head.appendChild(s);
+    });
+  }
+  load('app-core.js')
+    .then(function(){return load('app-config.js');})
+    .then(function(){return load('app-main.js');})
+    .catch(function(err){console.error('PPP-RL Startfehler',err);});
+})();
